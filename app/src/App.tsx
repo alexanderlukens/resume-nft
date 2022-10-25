673be6a6739a394b33e603586b29ecb6ccc00cc2
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-25 15:20:11 -0700

    begin work on deploying github page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THIS IS THE RESUME APP
         </a>
       </header>
     </div>
