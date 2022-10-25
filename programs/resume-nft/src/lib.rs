288f27af3fe4ea420d3c03c749e7e0e7692ea623
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-25 09:45:40 -0700

    setup mintnft struct

--- programs/resume-nft/src/lib.rs
+++ programs/resume-nft/src/lib.rs
@@ -12,10 +12,31 @@ declare_id!("7dGW6wNb8PjvBXazgTjWUAPoZJfU8aeDXsJW6xkETQDE");
 pub mod resume_nft {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint(ctx: Context<MintNft>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintNft<'info> {
+    /// CHECK: Metaplex will create this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: Metaplex will create this account
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub mint: Signer<'info>,
+    /// CHECK: Anchor will create this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+    /// CHECK: Metaplex will check this
+    pub token_metadata_program: UncheckedAccount<'info>,
+}
