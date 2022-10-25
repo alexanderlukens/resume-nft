97509f2295d2bd01f12607f7c7246ecee75ed768
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-25 11:24:51 -0700

    write mint test

--- tests/resume-nft.ts
+++ tests/resume-nft.ts
@@ -4,17 +4,58 @@ import { ResumeNft } from "../target/types/resume_nft";
 
 describe("resume-nft", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
   const nftTitle = "Alex's Resume";
   const nftSymbol = "ALEXLUKENS";
   const nftMetadataUrl = "https://raw.githubusercontent.com/alexanderlukens/resume-nft/main/assets/resume.json";
 
-  const program = anchor.workspace.ResumeNft as Program<ResumeNft>;
+  const provider = anchor.AnchorProvider.env()
+  const wallet = provider.wallet as anchor.Wallet;
+  anchor.setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.mint().rpc();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.ResumeNft as Program<ResumeNft>;
+  const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+
+  it("Can mint!", async () => {
+    const mintKeypair: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const tokenAddress = await anchor.utils.token.associatedAddress({
+      mint: mintKeypair.publicKey, 
+      owner: wallet.publicKey
+    })
+
+    const metadataAddress = (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from('metadata'),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mintKeypair.publicKey.toBuffer(),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0]
+
+    const masterEditionAddress = (await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("metadata"),
+        TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+        mintKeypair.publicKey.toBuffer(),
+        Buffer.from("edition"),
+      ],
+      TOKEN_METADATA_PROGRAM_ID
+    ))[0];
+
+
+    const tx = await program.methods.mint(nftTitle, nftSymbol, nftMetadataUrl)
+      .accounts({
+        masterEdition: masterEditionAddress,
+        metadata: metadataAddress,
+        mint: mintKeypair.publicKey,
+        tokenAccount: tokenAddress,
+        mintAuthority: wallet.publicKey,
+        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID
+      })
+      .signers([mintKeypair])
+      .rpc();
+    // console.log("Your transaction signature", tx);
   });
 });
