383a5d02268a5ef734c97266d9df4c5afaec73a9
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-25 09:30:25 -0700

    change program id

--- programs/resume-nft/src/lib.rs
+++ programs/resume-nft/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7dGW6wNb8PjvBXazgTjWUAPoZJfU8aeDXsJW6xkETQDE");
 
 #[program]
 pub mod resume_nft {
