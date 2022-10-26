9cb9a79f3795039a6276a57d086a20291ef344d1
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-25 18:14:47 -0700

    add wallet context

---
+++ app/src/context/WalletContext.tsx
@@ -0,0 +1,31 @@
+import { FC, ReactNode, useCallback, useMemo, useState} from 'react'
+import { ConnectionProvider, WalletProvider, useWallet } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+
+export const WalletContext: FC<{ children: ReactNode }> = ({ children }) => {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = clusterApiUrl(network)
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+        ],
+        []
+    );
+
+    const onError = useCallback((error: WalletError) => {
+        // custom handling for Slope since it doesn't return a message
+        console.error(error.name, error.message)
+    }, []);
+
+    return (
+        <ConnectionProvider endpoint={endpoint} config={{ confirmTransactionInitialTimeout: 240000 }}>
+            <WalletProvider wallets={wallets} onError={onError} autoConnect>
+                {children}
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
