947882485ab2eaf0251c8718332dfd106e7a11d9
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-26 09:29:31 -0700

    attempt to remove eslint

--- app/src/contexts/WalletContext.tsx
+++ app/src/contexts/WalletContext.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useCallback, useMemo} from 'react'
+import { FC, ReactNode, useCallback, useMemo } from 'react'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
@@ -7,7 +7,7 @@ import { clusterApiUrl } from '@solana/web3.js';
 
 export const WalletContext: FC<{ children: ReactNode }> = ({ children }) => {
     const network = WalletAdapterNetwork.Devnet;
-    const endpoint = clusterApiUrl(network)
+    const endpoint = clusterApiUrl(network);
 
     const wallets = useMemo(
         () => [
@@ -22,7 +22,10 @@ export const WalletContext: FC<{ children: ReactNode }> = ({ children }) => {
     }, []);
 
     return (
-        <ConnectionProvider endpoint={endpoint} config={{ confirmTransactionInitialTimeout: 240000 }}>
+        <ConnectionProvider
+            endpoint={endpoint}
+            config={{ confirmTransactionInitialTimeout: 240000 }}
+        >
             <WalletProvider wallets={wallets} onError={onError} autoConnect>
                 {children}
             </WalletProvider>
