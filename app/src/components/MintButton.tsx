7c889eac2c90c371107c672b7793550b68220426
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-26 15:06:22 -0700

    get first pass of minting working

--- app/src/components/MintButton.tsx
+++ app/src/components/MintButton.tsx
@@ -1,25 +1,68 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { FC, useState } from 'react'
+import * as anchor from '@project-serum/anchor'
 import Button from '@mui/material/Button'
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { LAMPORTS_PER_SOL } from '@solana/web3.js'
+import { useWallet } from '@solana/wallet-adapter-react'
 
 import useToast from '../hooks/useToast'
 import useMintProgram from '../hooks/useMintProgram'
 
+const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s'
+)
+
 const MintButton: FC = () => {
   const [loading, setLoading] = useState<boolean>(false)
-  const { connection } = useConnection()
   const wallet = useWallet()
   const mintProgram = useMintProgram()
 
   const { showSuccessToast, showErrorToast } = useToast()
 
+  if (!mintProgram) {
+    return null
+  }
+
   const onClick = async (): Promise<void> => {
     if (wallet.publicKey) {
       setLoading(true)
       try {
-        await connection.requestAirdrop(wallet.publicKey, 2 * LAMPORTS_PER_SOL)
+        const mintKeypair: anchor.web3.Keypair = anchor.web3.Keypair.generate()
+        const tokenAddress = await anchor.utils.token.associatedAddress({
+          mint: mintKeypair.publicKey,
+          owner: wallet.publicKey
+        })
+
+        const metadataAddress = (await anchor.web3.PublicKey.findProgramAddress(
+          [
+            Buffer.from('metadata'),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            mintKeypair.publicKey.toBuffer()
+          ],
+          TOKEN_METADATA_PROGRAM_ID
+        ))[0]
+
+        const masterEditionAddress = (await anchor.web3.PublicKey.findProgramAddress(
+          [
+            Buffer.from('metadata'),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            mintKeypair.publicKey.toBuffer(),
+            Buffer.from('edition')
+          ],
+          TOKEN_METADATA_PROGRAM_ID
+        ))[0]
+
+        await mintProgram.methods.mint()
+          .accounts({
+            masterEdition: masterEditionAddress,
+            metadata: metadataAddress,
+            mint: mintKeypair.publicKey,
+            tokenAccount: tokenAddress,
+            mintAuthority: wallet.publicKey,
+            tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID
+          })
+          .signers([mintKeypair])
+          .rpc()
+
         showSuccessToast('Mint Successful')
       } catch (e) {
         showErrorToast('Mint Failure.')
