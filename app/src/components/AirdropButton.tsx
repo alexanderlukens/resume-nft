4d3d8b5eb3bc258b5db989036ec02c12f1c8b89f
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-26 14:02:52 -0700

    disable button when pressed

--- app/src/components/AirdropButton.tsx
+++ app/src/components/AirdropButton.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Button from '@mui/material/Button'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import { LAMPORTS_PER_SOL } from '@solana/web3.js'
@@ -7,6 +7,7 @@ import { LAMPORTS_PER_SOL } from '@solana/web3.js'
 import useToast from '../hooks/useToast'
 
 const AirdropButton: FC = () => {
+  const [loading, setLoading] = useState<boolean>(false)
   const { connection } = useConnection()
   const wallet = useWallet()
 
@@ -14,17 +15,19 @@ const AirdropButton: FC = () => {
 
   const onClick = async (): Promise<void> => {
     if (wallet.publicKey) {
+      setLoading(true)
       try {
         await connection.requestAirdrop(wallet.publicKey, 2 * LAMPORTS_PER_SOL)
         showSuccessToast('Airdrop Successful')
       } catch (e) {
         showErrorToast('Airdrop Failure. Please try again later.')
       }
+      setLoading(false)
     }
   }
 
   return (
-    <Button onClick={onClick}>
+    <Button disabled={loading} onClick={onClick}>
       Airdrop 2 Sol
     </Button>
   )
