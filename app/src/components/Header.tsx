0f710b11144120395b6e3de88285aaa5c6a59f92
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-26 10:39:22 -0700

    add portfolio button to header

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react'
 import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 const Header: FC = () => {
   return (
-    <AppBar>
-      <Grid container>
+    <AppBar color="secondary">
+      <Grid container alignItems="center" justifyContent={'space-between'} p={1}>
+        <Grid item>
+          <Button variant='contained' href="https://alexanderlukens.github.io/">Back to Portfolio</Button>
+        </Grid>
+        <Grid item>
+          <Typography variant="h4">Alex&apos;s Resume NFT</Typography>
+        </Grid>
         <Grid item>
           <WalletMultiButton />
         </Grid>
