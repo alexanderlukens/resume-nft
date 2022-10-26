c618e4d477cf29a30ca1761d16a2b1070f5b81da
Alexander Lukens <alexander.lukens1@gmail.com>
2022-10-25 18:19:35 -0700

    add wallet context to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 
+import { WalletContext } from './contexts/WalletContext';
+
 function App() {
   return (
-    <Container maxWidth="lg">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          THIS IS THE RESUME APP
-        </a>
-      </header>
-    </Container>
+    <WalletContext>
+      <Container maxWidth="lg">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            THIS IS THE RESUME APP
+          </a>
+        </header>
+      </Container>
+    </WalletContext>
   );
 }
 
