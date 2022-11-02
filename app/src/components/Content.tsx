a96fad3a52bff15eb14a52fd4a5d496bb90bba63
Alexander Lukens <alexander.lukens1@gmail.com>
2022-11-02 16:10:03 -0700

    style nft card

--- app/src/components/Content.tsx
+++ app/src/components/Content.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 
@@ -38,42 +37,35 @@ const Content: FC = () => {
     <>
       <Box
         sx={{
-          bgcolor: 'background.paper',
           pt: 8,
           pb: 6
         }}
       >
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-            gutterBottom
-          >
-            Alex&apos;s Resume NFT
-          </Typography>
-          <Typography variant="h5" align="center" color="text.secondary" paragraph>
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so folks
-            don&apos;t simply skip over it entirely.
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <MintButton />
-            <AirdropButton />
-          </Stack>
-        </Container>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Alex&apos;s Resume NFT
+        </Typography>
+        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+          Something short and leading about the collection below—its contents,
+          the creator, etc. Make it short and sweet, but not too short so folks
+          don&apos;t simply skip over it entirely.
+        </Typography>
+        <Stack
+          sx={{ pt: 4 }}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+        >
+          <MintButton />
+          <AirdropButton />
+        </Stack>
       </Box>
-      <Grid container justifyContent="space-between">
-        <Grid item xs={12}>
-          <NFTDisplay />
-        </Grid>
-      </Grid>
+      <NFTDisplay />
     </>
   )
 }
--- app/src/components/NFTDisplay.tsx
+++ app/src/components/NFTDisplay.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardActionArea from '@mui/material/CardActionArea'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import ModalImage from 'react-modal-image'
@@ -15,7 +18,7 @@ const NFTDisplay: React.FC = () => {
 
   if (!nfts.length) {
     return (
-      <Grid container spacing={2}>
+      <Grid container spacing={2} sx={{ mb: 3 }}>
         <Grid item>
           <Typography>You dont have Alex&apos;s Resume :(</Typography>
           <Typography>Click the mint button to get one!</Typography>
@@ -25,15 +28,21 @@ const NFTDisplay: React.FC = () => {
   }
 
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} justifyContent="center" sx={{ mb: 3 }}>
       {nfts.map((nft, index) => {
         return (
           <Grid key={`nft-${index}`} item xs={4}>
-            <ModalImage
-              small={nft.imageUrl}
-              large={nft.imageUrl}
-            />
-            <Button onClick={() => openSolscan(nft.address)}>View on Solscan</Button>
+            <Card raised>
+              <CardActionArea disableRipple>
+                <ModalImage
+                  small={nft.imageUrl}
+                  large={nft.imageUrl}
+                />
+              </CardActionArea>
+              <CardActions sx={{ justifyContent: 'center' }}>
+                <Button onClick={() => openSolscan(nft.address)}>View on Solscan</Button>
+              </CardActions>
+            </Card>
           </Grid>
         )
       })}
