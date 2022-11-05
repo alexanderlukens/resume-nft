9e7d6d6cc48020a80f9d0e6b89efeacdbf0416c1
Alexander Lukens <alexander.lukens1@gmail.com>
2022-11-05 13:55:04 -0700

    add github repo link

--- app/src/components/Content.tsx
+++ app/src/components/Content.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
 import Box from '@mui/material/Box'
 import CircularProgress from '@mui/material/CircularProgress'
+import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
@@ -44,41 +46,49 @@ const Content: FC = () => {
 
   return (
     <Paper>
-      <Grid container justifyContent={'center'}>
+      <Container maxWidth="sm">
         <Box
           sx={{
             pt: 1,
             pb: 6
           }}
         >
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="text.primary"
-          >
-            Alex&apos;s Resume NFT
-          </Typography>
-          <Typography variant="h5" align="center" color="text.secondary">
-            An NFT project running on Solana (devnet)
-          </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            justifyContent="center"
-          >
-            <Typography variant="body1" align="center" color="text.secondary">
-              Getting Started
-            </Typography>
-            <Typography variant="overline" align="left" color="text.secondary">
-              1. Connect Wallet
-            </Typography>
-            <Typography variant="overline" align="left" color="text.secondary">
-              2. Airdrop Sol if balance is below 2 (Your Balance: <Typography color="text.primary" component="span" sx={{ fontWeight: 'bold' }} variant="overline">{balance} SOL</Typography>)
-            </Typography>
-            <Typography variant="overline" align="left" color="text.secondary">
-              3. Click Mint
-            </Typography>
-          </Stack>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+              >
+                Alex&apos;s Resume NFT
+              </Typography>
+              <Typography variant="h5" align="center" color="text.secondary" gutterBottom>
+                An NFT project running on Solana (devnet)
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body1" align="center" color="text.secondary" component="div">
+                Getting Started
+              </Typography>
+              <Typography variant="overline" align="left" color="text.secondary" component="div">
+                1. Connect Wallet
+              </Typography>
+              <Typography variant="overline" align="left" color="text.secondary" component="div">
+                2. Airdrop Sol if balance is below 2 (Your Balance: <Typography color="text.primary" component="span" sx={{ fontWeight: 'bold' }} variant="overline">{balance} SOL</Typography>)
+              </Typography>
+              <Typography variant="overline" align="left" color="text.secondary" component="div">
+                3. Click Mint
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Link href="https://github.com/alexanderlukens/resume-nft" target="_blank" rel="noopener">
+                <Typography variant="body1" align="center">
+                  Github Repo
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
           <Stack
             sx={{ pt: 4 }}
             direction="row"
@@ -89,9 +99,9 @@ const Content: FC = () => {
             <MintButton />
           </Stack>
         </Box>
-      </Grid>
+      </Container>
       {body}
-    </Paper>
+    </Paper >
   )
 }
 
